4e0e7e7a5f5d6d48e7137da99d647d09d64322f4
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-14 07:55:13 +0400

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -7,25 +7,24 @@
     overlays.default = final: prev: {
       helix = self.packages.${prev.system}.default;
     };
-    packages = utils.lib.eachSystem { } (pkgs:
-      let
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = let
         hx = import ./nix/hx.nix { inherit pkgs; };
         runtime = import ./nix/runtime.nix { inherit pkgs; };
-      in {
-        default = pkgs.writeShellApplication {
-          name = "hx";
-          runtimeInputs = [ pkgs.helix ] ++ runtime;
-          text = ''
-            HELIX_RUNTIME="${hx.hxRt}/lib/runtime" hx --config ${hx.hxConfig}/lib/config.toml "$@"
-          '';
-          meta = {
-            description = "A fully configured helix config";
-            homepage = "https://github.com/NewDawn0/hxConfig";
-            license = pkgs.lib.licenses.mit;
-            maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-            platforms = pkgs.lib.platforms.all;
-          };
+      in pkgs.writeShellApplication {
+        name = "hx";
+        runtimeInputs = [ pkgs.helix ] ++ runtime;
+        text = ''
+          HELIX_RUNTIME="${hx.hxRt}/lib/runtime" hx --config ${hx.hxConfig}/lib/config.toml "$@"
+        '';
+        meta = {
+          description = "Fully setup runnable helix configuration";
+          homepage = "https://github.com/NewDawn0/hxConfig";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
         };
-      });
+      };
+    });
   };
 }
